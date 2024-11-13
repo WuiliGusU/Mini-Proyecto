c4810a7eda866c7c8c7f64725886d6e12b5b3cc9
WuiliGusU <wsumalep@miumg.edu.gt>
2024-11-12 22:34:40 -0600

    Se agrego el frontend del miniproyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AgeChecker from './AgeChecker';
+
+function App() {
+    return (
+        <div className="App">
+            <AgeChecker />
+        </div>
+    );
+}
+
+export default App;
